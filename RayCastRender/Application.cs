51dd43a8b834803b41d8ede646ca12ff1dc7d283
Kraaven <dhruv.bhojaraj@gmail.com>
2024-12-01 12:37:49 +0530

    Hello World Setup

---
+++ RayCastRender/Application.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs; // Import the Raylib library
+
+class Program
+{
+    static void Main()
+    {
+        Raylib.InitWindow(800, 600, "Raylib C# - Simple Window");
+        
+        Raylib.SetTargetFPS(60);
+        while (!Raylib.WindowShouldClose()) // Check if the window should close
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Hello, Raylib!", 350, 280, 20, Color.DarkGray);
+            Raylib.EndDrawing();
+        }
+        
+        Raylib.CloseWindow();
+    }
+}
